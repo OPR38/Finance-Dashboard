19cf20f0e32e8eb9919986752b4be2492a0402d0
OPR38 <pei_rong92@hotmail.com>
2024-11-09 11:56:12 +0800

    added workflow and modified

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Lesson-9/',
+  base: '/testing-currency/',
 
 })
